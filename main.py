688b9506d74b0b557183a1cb0d418b11119e948c
Alex Cleveland <alexcleveland5@gmail.com>
2026-02-13 21:38:37 -0700

    Start of function to tell if it is dark out, if so call ISS api and see where its at

--- main.py
+++ main.py
@@ -3,25 +3,33 @@ from datetime import datetime
 
 MY_LAT = 39.6136
 MY_LONG = -105.0165
-# response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# #Raise Exceptions for Errors
-# response.raise_for_status()
-#
-# longitude = response.json()["iss_position"]["longitude"]
-# latitude = response.json()["iss_position"]["latitude"]
-# iss_position = (longitude, latitude)
-# print(iss_position)
 
 
-parameters = {"lat" : MY_LAT,
-              "lng": MY_LONG,
-              "formatted": 0}
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
-print(sunrise, sunset)
-time_now = datetime.now()
-print(time_now.hour)
+
+
+
+
+def is_iss_overhead(MY_LAT, MY_LONG):
+    parameters = {"lat": MY_LAT,
+                  "lng": MY_LONG,
+                  "formatted": 0}
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now()
+    current_hour = time_now.hour
+
+    if (sunset + 24) > current_hour > sunrise:
+        # iss
+        iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
+        # Raise Exceptions for Errors
+        iss_response.raise_for_status()
+        longitude = float(iss_response.json()["iss_position"]["longitude"])
+        latitude = float(iss_response.json()["iss_position"]["latitude"])
+        iss_position = (longitude, latitude)
+
+
+
